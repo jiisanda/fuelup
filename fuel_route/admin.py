4935013b6a1ce51a932c05b0f0cc12809baef404
jiisanda <harshjaiswal2307@gmail.com>
2024-11-28 00:13:39 +0530

    admin view update

--- fuel_route/admin.py
+++ fuel_route/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from fuel_route.models import TruckStop
 
-# Register your models here.
-admin.site.register(TruckStop)
+
+@admin.register(TruckStop)
+class TruckStopAdmin(admin.ModelAdmin):
+    list_display = ("opis_id", "name", "address", "city", "state", "rack_id", "retail_price")
