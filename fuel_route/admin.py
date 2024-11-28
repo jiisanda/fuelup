320949f428f72b399cdafa861bea4dba18d6cea1
jiisanda <harshjaiswal2307@gmail.com>
2024-11-29 01:11:23 +0530

    admin view update

--- fuel_route/admin.py
+++ fuel_route/admin.py
@@ -5,4 +5,4 @@ from fuel_route.models import TruckStop
 
 @admin.register(TruckStop)
 class TruckStopAdmin(admin.ModelAdmin):
-    list_display = ("opis_id", "name", "address", "city", "state", "rack_id", "retail_price")
+    list_display = ("opis_id", "name", "address", "city", "state", "rack_id", "retail_price", "latitude", "longitude")
